088d8b90545a4c25ef09015bb2233baa8ec7e564
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-09-20 17:15:07 +0530

    Change3

--- Learning/Leet_Code_Solutions/Trees/107-Binary-Tree-Level-Order-Traversal-II.cpp
+++ Learning/Leet_Code_Solutions/Trees/107-Binary-Tree-Level-Order-Traversal-II.cpp
@@ -11,30 +11,50 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    // Tip : Whenever you are solving any question were you have to remember the next levels of the current node
+    // use a queue generically. It may be tree question or a graph question.
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        // The Levels of a Binary Tree stored in the 2d - vector
         vector<vector<int>> arr;
+        // If root is null return the empty vector
         if(root == NULL) return arr;
+        // For storing the TreeNodes we are declaring a queue of TreeNode pointers
         queue<TreeNode*> q;
+        // We push the root at first.
         q.push(root);
+        // q.push(NULL);
+        // Storing the contents of the current level in the level vector.
         vector<int> level;
-        stack<vector<int>> st;
+        // While the queue is not empty we have a level remaining so we enter the while loop.
         while(!q.empty()){
+            // if(current == NULL){
+            //     arr.push_back(level);
+            //     level.clear();
+            //     q.pop();
+            //     if(q.empty()){
+            //         break;
+            //     }
+            //     q.push(NULL);
+            //     continue;
+            // }
+            // Using this approach at any instant we only have a specific level in the Binary Tree.
+            // We just check the no. of elements in the queue and we push the node values in the level vector 
+            // as well we start filling the next level in the queue but as the for loop is iterating for just the 
+            // n - elements we don't pop the next level elements or add them into the level vector.
             int n = q.size();
             for(int i = 0;i < n;i++){
                 TreeNode* current = q.front();
                 level.push_back(current->val);
-                if(current->left) q.push(current->left);
-                if(current->right) q.push(current->right);
+                if(current->left != NULL) q.push(current->left);
+                if(current->right != NULL) q.push(current->right);
                 q.pop();
             }
-            st.push(level);
+            // We push the current level into the 2-d vector.
+            arr.push_back(level);
             level.clear();
         }
-        while(!st.empty()){
-            auto it = st.top();
-            arr.push_back(it);
-            st.pop();
-        }
+        // We return the 2-d vector answer.
         return arr;
     }
 };
