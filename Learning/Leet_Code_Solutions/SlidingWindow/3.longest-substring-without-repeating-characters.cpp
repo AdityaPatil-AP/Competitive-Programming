73bbd23f2e509fe70cba4b35668cc5c88bf4146d
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-10-08 08:23:49 +0530

    commit1

--- Learning/Leet_Code_Solutions/SlidingWindow/3.longest-substring-without-repeating-characters.cpp
+++ Learning/Leet_Code_Solutions/SlidingWindow/3.longest-substring-without-repeating-characters.cpp
@@ -31,3 +31,40 @@ public:
         return mx;
     }
 };
+
+// Alternative Solution without using a hashmap
+
+int longestUniqueSubsttr(string s){
+        //code
+        // Variable Sliding Window Question
+        int i = 0, j = 0;
+        // They are asking for the unique characters.
+        int size = s.size();
+        // characters must be stored in some set or map, let's use map it can be done using set as well.
+        int arr[26] = {0};
+        int count = 0;
+        int mx = 0;
+        while(j < size){
+            // Calculations
+            arr[s[j] - 'a']++;
+            if(arr[s[j] - 'a'] == 1){
+                count++;
+            }
+            if(count == (j - i + 1)){
+                // ans<-calculations
+                mx = max(mx, (j - i + 1));
+                j++;
+            }
+            else if(count < (j - i + 1)){
+                if(count < (j - i + 1)){
+                    arr[s[i] - 'a']--;
+                    if(arr[s[i] - 'a'] == 0){
+                        count--;
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return mx;
+    }
