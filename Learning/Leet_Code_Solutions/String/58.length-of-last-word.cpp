a04aaa7cbff2b6a6265fa3b589017880ac1b17ad
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-10-12 11:10:39 +0530

    commit1

--- Learning/Leet_Code_Solutions/String/58.length-of-last-word.cpp
+++ Learning/Leet_Code_Solutions/String/58.length-of-last-word.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans = 0;
-        bool letterFound = false;
-        for(int i = s.size() - 1;i >= 0;i--){
-            if(s[i] == ' ' && letterFound){
-                break;
-            }
-            else if(s[i] != ' '){
-                letterFound = true;
-                ans++;
-            }
+        // int ans = 0;
+        // bool letterFound = false;
+        // for(int i = s.size() - 1;i >= 0;i--){
+        //     if(s[i] == ' ' && letterFound){
+        //         break;
+        //     }
+        //     else if(s[i] != ' '){
+        //         letterFound = true;
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+        
+        // skipping all trailing spaces.
+        int j = s.size() - 1;
+        while(j >= 0 && s[j] == ' '){
+            j--;
         }
-        return ans;
+        int i = j;
+        while(j >= 0 && s[j] != ' '){
+            j--;
+        }
+        return (i - j);
     }
 };
