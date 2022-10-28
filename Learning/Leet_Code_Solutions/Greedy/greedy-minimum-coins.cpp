37322614a11a568229102f030acdf9777da98561
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-10-28 15:54:30 +0530

    commit6

--- Learning/Leet_Code_Solutions/Greedy/greedy-minimum-coins.cpp
+++ Learning/Leet_Code_Solutions/Greedy/greedy-minimum-coins.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<int> ans1;
 int solve(int value){
     int denominations[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
     int ans = 0;
@@ -8,6 +9,7 @@ int solve(int value){
         while(denominations[i] <= value){
             value -= denominations[i];
             ans++;
+            ans1.push_back(denominations[i]);
         }
     }
     return ans;
@@ -17,5 +19,9 @@ int main(){
     int value;
     cin >> value;
     cout << solve(value) << endl;
+    for(int i = 0;i < ans1.size();i++){
+        cout << ans1[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
