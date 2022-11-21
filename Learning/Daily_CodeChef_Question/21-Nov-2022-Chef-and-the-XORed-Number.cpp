461774cfae193aef25996cf1782f55511d390ac1
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-11-21 20:29:32 +0530

    commit1

---
+++ Learning/Daily_CodeChef_Question/21-Nov-2022-Chef-and-the-XORed-Number.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <chrono>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+#define mod 1000000007
+#define ll long long int
+#define PRIME 1000000007
+
+const int N = 1000000;
+#define pb push_back
+
+bool powerOf2(ll m){
+    // ll n = __builtin_popcountll(m);
+    // return n == 1;
+    return !(m & (m - 1));
+}
+
+void work(){
+    // Odd Divisors.
+    ll n;
+    cin >> n;
+    ll m = n + 1;
+    if(n == 1){
+        cout << 2 << endl;
+        return;
+    }
+    bool yes = powerOf2(m);
+    if(yes) cout << n/2 << endl;
+    else cout << -1 << endl;
+    return;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("E:\\only one folder\\CompetitiveProgramming Github Repo\\Competitive-Programming\\Learning\\input.txt","r", stdin);
+    freopen("E:\\only one folder\\CompetitiveProgramming Github Repo\\Competitive-Programming\\Learning\\output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int testcase = 1;
+    cin >> testcase;
+    for(int i = 0;i < testcase;i++){
+        work();
+    }
+    return 0;
+}
+
+
