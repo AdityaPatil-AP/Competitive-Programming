0de67bddff0b1ece140f28af377cb6b10c905b18
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-11-22 11:34:26 +0530

    commit2

---
+++ Learning/Leet_Code_Solutions/DynamicProgramming/121.best-time-to-buy-and-sell-a-stock.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    // 1) Kadane's Algorithm
+    // int maxProfit(vector<int>& prices) {
+    //     int profit = 0;
+    //     int minelem = prices[0];
+    //     for(int i = 1; i < prices.size();i++){
+    //         minelem = min(minelem, prices[i]);
+    //         profit = max(profit, (prices[i] - minelem));
+    //     }
+    //     return profit;
+    // }
+    // 2) 2nd Approach
+
+    int maxProfit(vector<int> &prices)
+    {
+        int profit = 0;
+        int minelem = prices[0];
+        for (int i = 1; i < prices.size(); i++)
+        {
+            minelem = min(minelem, prices[i]);
+            profit = max(profit, (prices[i] - minelem));
+        }
+
+        return profit;
+    }
+};
