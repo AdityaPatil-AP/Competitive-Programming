4e3befd3ed595d144cc709e50ea32aadfe6ce1aa
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-02-20 15:41:40 +0530

    commit

--- Learning/Leet_Code_Solutions/TreesAndBinaryTrees/652.find-duplicate-subtrees.cpp
+++ Learning/Leet_Code_Solutions/TreesAndBinaryTrees/652.find-duplicate-subtrees.cpp
@@ -12,28 +12,51 @@
 class Solution
 {
 public:
-    string dfs(TreeNode *root, map<string, int> &mp, vector<TreeNode *> &ans)
+    map<int, int> cnt;
+    int dfs(TreeNode *root, map<string, int> &mp, vector<TreeNode *> &ans)
     {
         if (root == NULL)
-            return "";
+            return 0;
 
         // first we will create the string representation for the left and the right child subtree.
-        string left = dfs(root->left, mp, ans);
-        string right = dfs(root->right, mp, ans);
+        int left = dfs(root->left, mp, ans);
+        int right = dfs(root->right, mp, ans);
 
-        string curr = "(" + left + ")" + to_string(root->val) + "(" + right + ")";
+        // Creation of this string would take O(n) time.
+        // and there are O(n) states so the time complexity will be O(n^2).
+        // string curr = "(" + left + ")" + to_string(root->val) + "(" + right + ")";
 
-        mp[curr]++;
+        // Here we have proposed another technique which will do the above verification but in constant time.
+        // We can keep a triplet instead of a string. The triplet will contain left subtree's id, root->val,
+        // and right subtrees id. Every node will get an id for itself. If the same id is repeated twice.
+        // Then we can add it to the ans because two nodes have the same structure meaning they are duplicate.
 
-        if (mp[curr] == 2)
+        string triplet = to_string(left) + "," + to_string(root->val) + "," + to_string(right);
+
+        if (!mp.count(triplet))
+        {
+            mp[triplet] = mp.size() + 1;
+        }
+        int id = mp[triplet];
+
+        cnt[id]++;
+
+        // mp[curr]++;
+
+        if (cnt[id] == 2)
         {
             ans.push_back(root);
         }
-        return curr;
+        return id;
     }
 
     vector<TreeNode *> findDuplicateSubtrees(TreeNode *root)
     {
+        // Here the only thing to understand was every node's subtree can be represented in the form
+        // of a string. If the string occurences are stored in map and if they are occuring more than
+        // once then the particular node is leading to a Duplicate Subtree.
+        // Store it in the answer array.
+
         map<string, int> mp;
         vector<TreeNode *> ans;
 
