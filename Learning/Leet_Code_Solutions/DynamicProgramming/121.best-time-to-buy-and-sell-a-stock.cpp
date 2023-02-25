48862482969bce57ef0b9c8cea0e15ff6aff5d0c
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-02-25 12:04:55 +0530

    commit

--- Learning/Leet_Code_Solutions/DynamicProgramming/121.best-time-to-buy-and-sell-a-stock.cpp
+++ Learning/Leet_Code_Solutions/DynamicProgramming/121.best-time-to-buy-and-sell-a-stock.cpp
@@ -15,12 +15,21 @@ public:
 
     int maxProfit(vector<int> &prices)
     {
-        int profit = 0;
+        //         int profit = 0;
+        //         int minelem = prices[0];
+        //         for(int i = 1;i < prices.size();i++){
+        //             minelem = min(minelem, prices[i]);
+        //             profit = max(profit, (prices[i] - minelem));
+        //         }
+
+        //         return profit;
+
         int minelem = prices[0];
+        int profit = 0;
         for (int i = 1; i < prices.size(); i++)
         {
-            minelem = min(minelem, prices[i]);
-            profit = max(profit, (prices[i] - minelem));
+            profit = max(profit, prices[i] - minelem);
+            minelem = min(prices[i], minelem);
         }
 
         return profit;
