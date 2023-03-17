66169a0b408fd83422f28574baa6b2323401ce7c
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-03-17 22:54:24 +0530

    commit

--- Learning/Leet_Code_Solutions/Trie/676.implement-magic-dictionary.cpp
+++ Learning/Leet_Code_Solutions/Trie/676.implement-magic-dictionary.cpp
@@ -40,3 +40,95 @@ public:
         return false;
     }
 };
+
+struct Node
+{
+    Node *links[26];
+    bool isLeaf;
+
+    Node()
+    {
+        isLeaf = false;
+        for (int i = 0; i < 26; i++)
+        {
+            links[i] = NULL;
+        }
+    }
+};
+
+// Using Trie.
+
+class MagicDictionary
+{
+private:
+    Node *root;
+
+public:
+    MagicDictionary()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (node->links[word[i] - 'a'] == NULL)
+            {
+                node->links[word[i] - 'a'] = new Node();
+            }
+
+            node = node->links[word[i] - 'a'];
+        }
+
+        node->isLeaf = true;
+    }
+
+    void buildDict(vector<string> dict)
+    {
+        for (auto str : dict)
+        {
+            insert(str);
+        }
+    }
+
+    bool search(string searchWord)
+    {
+        Node *node = root;
+        return search(0, searchWord, false, node);
+    }
+
+    bool search(int index, string &word, bool change, Node *node)
+    {
+        if (index == word.size())
+        {
+            if (node->isLeaf && change)
+                return true;
+            return false;
+        }
+
+        if (!change)
+        {
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                if (ch == word[index])
+                {
+                    continue;
+                }
+                if (node->links[ch - 'a'] != NULL)
+                {
+                    if (search(index + 1, word, true, node->links[ch - 'a']))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        if (!node->links[word[index] - 'a'])
+        {
+            return false;
+        }
+        return search(index + 1, word, change, node->links[word[index] - 'a']);
+    }
+};
