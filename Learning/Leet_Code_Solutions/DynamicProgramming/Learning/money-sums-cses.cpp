55ffbe85812eb784d6e782b0428a5436b3b3ea05
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-04-04 10:52:15 +0530

    commit

--- Learning/Leet_Code_Solutions/DynamicProgramming/Learning/money-sums-cses.cpp
+++ Learning/Leet_Code_Solutions/DynamicProgramming/Learning/money-sums-cses.cpp
@@ -3,39 +3,57 @@ using namespace std;
 
 #define mod 1000000007
 #define ll long long int
+#define macro int
 
 int main()
 {
 
     // Checking for a possibility question.
-    int n;
+    ll n;
     cin >> n;
-    vector<int> arr(n);
+    vector<ll> arr(n);
     for (auto &x : arr)
         cin >> x;
 
-    vector<vector<int>> dp(n + 1, vector<int>(1e5 + 1, false));
+    // vector<vector<int>> dp(n + 1, vector<int>(1e5 + 1, false));
 
-    dp[0][0] = true;
+    // Space Optimisation.
+    vector<ll>
+        prev(1e5 + 1, false);
+
+    prev[0] = true;
 
     for (int i = 1; i <= n; i++)
     {
+        vector<ll> curr(1e5 + 1, false);
+        int currDenomination = arr[i - 1];
         for (int val = 0; val <= 1e5; val++)
         {
-            // I take the ith index and add the sum.
-            if (val - arr[i - 1] >= 0)
+            if (prev[val])
             {
-                dp[i][val] = dp[i - 1][val - arr[i - 1]];
+                // If val was possible for the last denomination.
+                // the we have two options for it.
+                // either take it or leave it.
+
+                // 1) take it.
+                int newval = val + currDenomination;
+                if (newval <= 1e5 && newval >= 0)
+                {
+                    curr[newval] = true;
+                }
+
+                // not_take.
+                if (val >= 0)
+                    curr[val] = true;
             }
-            // or i decide to not take the current coin.
-            dp[i][val] = (dp[i][val] || (dp[i - 1][val]));
         }
+        prev = curr;
     }
 
-    vector<int> ans;
+    vector<ll> ans;
     for (int i = 1; i <= 1e5; i++)
     {
-        if (dp[n][i])
+        if (prev[i])
         {
             ans.push_back(i);
         }
