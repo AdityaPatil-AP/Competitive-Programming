527f8e2315c847f686d4d0e2cf5b308396ee042e
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-05-09 16:34:06 +0530

    commit

--- Learning/Leet_Code_Solutions/Matrix/54.spiral-matrix.cpp
+++ Learning/Leet_Code_Solutions/Matrix/54.spiral-matrix.cpp
@@ -1,54 +1,52 @@
-
-
-// Using recursion.
-
 class Solution
 {
 public:
     vector<int> spiralOrder(vector<vector<int>> &matrix)
     {
-        // ending point (n/2, m/2);
         int n = matrix.size();
         int m = matrix[0].size();
-        return order(matrix, 0, 0, n - 1, m - 1);
-    }
 
-    vector<int> order(vector<vector<int>> &matrix, int starti, int startj, int endi, int endj)
-    {
-        if (starti > endi || startj > endj)
-        {
-            return {};
-        }
-        if (starti == endi)
-        {
-            vector<int> curr;
-            for (int i = startj; i <= endj; i++)
-            {
-                curr.push_back(matrix[starti][i]);
-            }
-            return curr;
-        }
+        int rowstart = 0;
+        int colstart = 0;
 
-        vector<int> smallans = order(matrix, starti + 1, startj + 1, endi - 1, endj - 1);
+        int rowend = n - 1;
+        int colend = m - 1;
 
         vector<int> ans;
-        // for the top row.
-        for (int i = startj; i <= endj; i++)
-            ans.push_back(matrix[starti][i]);
 
-        for (int i = starti + 1; i <= endi; i++)
-            ans.push_back(matrix[i][endj]);
-
-        for (int i = endj - 1; i >= startj; i--)
-            ans.push_back(matrix[endi][i]);
-
-        if (startj != endj)
-            for (int i = endi - 1; i >= starti + 1; i--)
-                ans.push_back(matrix[i][startj]);
-
-        for (auto it : smallans)
+        while (rowstart <= rowend && colstart <= colend)
         {
-            ans.push_back(it);
+            // right.
+            for (int i = colstart; i <= colend; i++)
+            {
+                ans.push_back(matrix[rowstart][i]);
+            }
+            rowstart++;
+
+            // down.
+            for (int i = rowstart; i <= rowend; i++)
+            {
+                ans.push_back(matrix[i][colend]);
+            }
+            colend--;
+
+            // left.
+            if (rowend < rowstart)
+                break;
+            for (int i = colend; i >= colstart; i--)
+            {
+                ans.push_back(matrix[rowend][i]);
+            }
+            rowend--;
+
+            // up.
+            if (colend < colstart)
+                break;
+            for (int i = rowend; i >= rowstart; i--)
+            {
+                ans.push_back(matrix[i][colstart]);
+            }
+            colstart++;
         }
 
         return ans;
