a6788bd205065731afea61929ea3772cc5c37bf7
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-05-30 16:00:17 +0530

    commit

--- Learning/Leet_Code_Solutions/DynamicProgramming/1547.minimum-cost-to-cut-a-stick.cpp
+++ Learning/Leet_Code_Solutions/DynamicProgramming/1547.minimum-cost-to-cut-a-stick.cpp
@@ -4,21 +4,52 @@ private:
 public:
     int minCost(int n, vector<int> &cuts)
     {
-        int m = cuts.size();
+        // int m = cuts.size();
+        // cuts.push_back(0);
+        // cuts.push_back(n);
+        // sort(cuts.begin(), cuts.end());
+
+        //         vector<vector<int>> dp(m + 2, vector<int>(m + 2, -1));
+
+        //         int ans = helper(0, m + 1, cuts, dp);
+
+        //         return ans;
+
+        // Bottom up Approach.
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
 
-        vector<vector<int>> dp(m + 2, vector<int>(m + 2, -1));
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
 
-        int ans = helper(0, m + 1, cuts, dp);
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = i; j <= (m - 1); j++)
+            {
+                // Base Case
+                if ((i + 1) >= j)
+                {
+                    dp[i][j] = 0;
+                }
+                else
+                {
+                    int ans = 1e9;
+                    for (int k = i + 1; k < j; k++)
+                    {
+                        int cost = dp[i][k] + dp[k][j] + (cuts[j] - cuts[i]);
+                        ans = min(ans, cost);
+                    }
+                    dp[i][j] = ans;
+                }
+            }
+        }
 
-        return ans;
+        return dp[0][m - 1];
     }
 
     int helper(int start, int end, vector<int> &cuts, vector<vector<int>> &dp)
     {
-        // Base Case
         if ((start + 1) == end)
         {
             return 0;
--- Learning/Leet_Code_Solutions/demo.cpp
+++ Learning/Leet_Code_Solutions/demo.cpp
@@ -4,22 +4,87 @@ using namespace std;
 #define ll long long int
 #define mod 1000000007
 
-int main()
+int countSubs(string &word)
 {
-    int n;
-    cin >> n;
-    vector<int> dp(n + 1, 0);
-    dp[0] = 1;
-    for (int i = 1; i <= n; i++)
+    int subs = 0;
+    for (int i = 1; i < word.size(); i++)
     {
-        for (int j = 1; j <= 6; j++)
+        if (word[i] == word[i - 1])
         {
-            if (i - j < 0)
-                break;
-            dp[i] = (dp[i] + dp[i - j]) % mod;
+            word[i] = ' ';
+            subs++;
         }
     }
+    return subs;
+}
 
-    cout << dp[n] << endl;
+vector<int> getPrefixScores(vector<int> &arr)
+{
+    vector<int> answer;
+    int n = arr.size();
+
+    vector<ll> prefixSum(n);
+    vector<ll> totalSum(n);
+    totalSum[0] = arr[0];
+    prefixSum[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefixSum[i] = prefixSum[i - 1] + arr[i];
+        totalSum[i] = prefixSum[i] + totalSum[i - 1];
+    }
+
+    priority_queue<ll> pq;
+    for (int k = 1; k <= n; k++)
+    {
+
+        pq.push(arr[k - 1]);
+
+        ll maxnum = pq.top();
+        ll sum = totalSum[k - 1] + (maxnum * k);
+
+        sum = sum % mod;
+        answer.push_back(sum);
+    }
+
+    return answer;
+}
+
+// #define ll long long int
+// #define mod 1000000007
+
+// vector<int> getPrefixScores(vector<int> &arr)
+// {
+//     vector<int> answer;
+//     int n = arr.size();
+//     multiset<ll> prefixSet; // Using multiset instead of priority_queue
+//     ll prefixSum = 0;
+//     for (int k = 1; k <= n; k++)
+//     {
+//         prefixSet.insert(arr[k - 1]);
+//         prefixSum += arr[k - 1];
+
+//         ll sum = 0;
+//         for (auto it = prefixSet.rbegin(); it != prefixSet.rend(); ++it)
+//         {
+//             sum += prefixSum;
+//             prefixSum -= *it;
+//         }
+
+//         sum = sum % mod;
+//         answer.push_back(sum);
+//     }
+
+//     return answer;
+// }
+
+int main()
+{
+    vector<int> words = {1, 2, 1};
+    vector<int> ans = getPrefixScores(words);
+    for (auto i : ans)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
