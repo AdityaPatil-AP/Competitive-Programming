7e08187e72b6f0b51cd1580fe592018290773b7c
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-06-09 11:06:21 +0530

    commit

--- Learning/Leet_Code_Solutions/BinarySearch/1608.special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ Learning/Leet_Code_Solutions/BinarySearch/1608.special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -3,41 +3,48 @@ class Solution
 public:
     int specialArray(vector<int> &nums)
     {
-        int n = nums.size();
+        // Time Complexity : O(logn)
+        //         int n = nums.size();
+        //         sort(nums.begin(), nums.end());
+        //         int l = 1, r = nums.size();
+
+        //         if(n <= nums[0]){
+        //             return n;
+        //         }
+
+        //         while(l < r){
+        //             int mid = (l + (r - l)/2);
+
+        //             int check = n - mid;
+
+        //             // check if the current mid is the answer.
+        //             if((check - 1) >= 0 && nums[check] >= mid && nums[check - 1] < mid){
+        //                 return mid;
+        //             }
+
+        //             if(nums[check] >= (mid)){
+        //                 l = mid + 1;
+        //             }
+        //             else{
+        //                 r = mid;
+        //             }
+        //         }
+        //         return -1;
+
+        // Using lower bound : Time Complexity : O(n*logn).
+
         sort(nums.begin(), nums.end());
-        int l = 1, r = nums.size();
 
-        if (n <= nums[0])
+        for (int i = 1; i <= nums.size(); i++)
         {
-            return n;
+            auto it = lower_bound(nums.begin(), nums.end(), i);
+
+            if (distance(it, nums.end()) == i)
+            {
+                return i;
+            }
         }
 
-        while (l < r)
-        {
-            int mid = (l + (r - l) / 2);
-
-            int check = n - mid;
-
-            // check if the current mid is the answer.
-            if ((check - 1) >= 0 && nums[check] >= mid && nums[check - 1] < mid)
-            {
-                return mid;
-            }
-
-            if (check == 0 && nums[check] >= mid)
-            {
-                return mid;
-            }
-
-            if (nums[check] >= (mid))
-            {
-                l = mid + 1;
-            }
-            else
-            {
-                r = mid;
-            }
-        }
         return -1;
     }
 };
