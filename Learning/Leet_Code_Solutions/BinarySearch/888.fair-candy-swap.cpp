e8ac05bda872b97988fcff33ab0f97828d1613bc
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-06-09 11:35:21 +0530

    commit

--- Learning/Leet_Code_Solutions/BinarySearch/888.fair-candy-swap.cpp
+++ Learning/Leet_Code_Solutions/BinarySearch/888.fair-candy-swap.cpp
@@ -3,41 +3,55 @@ class Solution
 public:
     vector<int> fairCandySwap(vector<int> &aliceSizes, vector<int> &bobSizes)
     {
+        // Using Binary Search and accumulate.
+        //         int aliceTotal = accumulate(aliceSizes.begin(), aliceSizes.end(), 0);
+        //         int bobTotal = accumulate(bobSizes.begin(), bobSizes.end(), 0);
+
+        //         sort(aliceSizes.begin(), aliceSizes.end());
+        //         sort(bobSizes.begin(), bobSizes.end());
+
+        //         if(aliceTotal > bobTotal){
+        //             int diff = aliceTotal - bobTotal;
+
+        //             for(int i = 0;i < bobSizes.size();i++){
+        //                 int curr = bobSizes[i];
+        //                 int toFind = curr + diff/2;
+
+        //                 if(find(aliceSizes, toFind)){
+        //                     return {toFind, curr};
+        //                 }
+        //             }
+        //         }
+        //         else{
+        //             int diff = bobTotal - aliceTotal;
+
+        //             for(int i = 0;i < aliceSizes.size();i++){
+        //                 int curr = aliceSizes[i];
+        //                 int toFind = curr + diff/2;
+
+        //                 if(find(bobSizes, toFind)){
+        //                     return {curr, toFind};
+        //                 }
+        //             }
+        //         }
+
+        //         return {-1, -1};
+
+        // Using Set.
         int aliceTotal = accumulate(aliceSizes.begin(), aliceSizes.end(), 0);
         int bobTotal = accumulate(bobSizes.begin(), bobSizes.end(), 0);
 
-        sort(aliceSizes.begin(), aliceSizes.end());
-        sort(bobSizes.begin(), bobSizes.end());
-
-        if (aliceTotal > bobTotal)
+        set<int> bobElem;
+        for (int i = 0; i < bobSizes.size(); i++)
         {
-            int diff = aliceTotal - bobTotal;
-
-            for (int i = 0; i < bobSizes.size(); i++)
-            {
-                int curr = bobSizes[i];
-                int toFind = curr + diff / 2;
-
-                if (find(aliceSizes, toFind))
-                {
-                    return {toFind, curr};
-                }
-            }
+            bobElem.insert(bobSizes[i]);
         }
-        else
+
+        for (int i = 0; i < aliceSizes.size(); i++)
         {
-            int diff = bobTotal - aliceTotal;
-
-            for (int i = 0; i < aliceSizes.size(); i++)
-            {
-                int curr = aliceSizes[i];
-                int toFind = curr + diff / 2;
-
-                if (find(bobSizes, toFind))
-                {
-                    return {curr, toFind};
-                }
-            }
+            int toFind = (bobTotal - aliceTotal) / 2 + aliceSizes[i];
+            if (bobElem.find(toFind) != bobElem.end())
+                return {aliceSizes[i], toFind};
         }
 
         return {-1, -1};
