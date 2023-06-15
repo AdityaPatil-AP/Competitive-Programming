7777f9923ddeb5e42c0f4cb876948fb0ef2ab3ba
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-06-15 13:08:16 +0530

    commit

--- Learning/Leet_Code_Solutions/TreesAndBinaryTrees/1161-Maximum-Level-Sum-Of-A-Binary-Tree.cpp
+++ Learning/Leet_Code_Solutions/TreesAndBinaryTrees/1161-Maximum-Level-Sum-Of-A-Binary-Tree.cpp
@@ -9,31 +9,71 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int maxLevelSum(TreeNode* root) {
-        if(root == NULL) return 0;
-        vector<int> levelSum;
-        queue<TreeNode*> q;
+    int maxLevelSum(TreeNode *root)
+    {
+        // if(root == NULL) return 0;
+        // vector<int> levelSum;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     int sum = 0;
+        //     for(int i = 0;i < n;i++){
+        //         TreeNode* current = q.front();
+        //         sum += current->val;
+        //         if(current->left) q.push(current->left);
+        //         if(current->right) q.push(current->right);
+        //         q.pop();
+        //     }
+        //     levelSum.push_back(sum);
+        // }
+        // int level = 0;
+        // for(int i = 1;i < levelSum.size();i++){
+        //     if(levelSum[i] > levelSum[level]){
+        //         level = i;
+        //     }
+        // }
+        // return level + 1;
+
+        if (root == NULL)
+            return 0;
+
+        queue<TreeNode *> q;
+
+        int ans = 1;
+        int max_sum = INT_MIN;
+        int level = 1;
         q.push(root);
-        while(!q.empty()){
+
+        while (!q.empty())
+        {
             int n = q.size();
+
             int sum = 0;
-            for(int i = 0;i < n;i++){
-                TreeNode* current = q.front();
-                sum += current->val;
-                if(current->left) q.push(current->left);
-                if(current->right) q.push(current->right);
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *curr = q.front();
                 q.pop();
+
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+                sum += curr->val;
             }
-            levelSum.push_back(sum);
-        }
-        int level = 0;
-        for(int i = 1;i < levelSum.size();i++){
-            if(levelSum[i] > levelSum[level]){
-                level = i;
+
+            if (sum > max_sum)
+            {
+                ans = level;
+                max_sum = sum;
             }
+
+            level++;
         }
-        return level + 1;
+
+        return ans;
     }
 };
