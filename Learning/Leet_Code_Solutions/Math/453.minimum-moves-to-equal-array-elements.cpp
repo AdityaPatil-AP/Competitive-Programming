b7ffaca40f20c2b3137c3c73a175e45fbb2b3ffa
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-06-21 19:29:21 +0530

    commit

---
+++ Learning/Leet_Code_Solutions/Math/453.minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int minMoves(vector<int> &nums)
+    {
+        // Find the difference of the largest with everyone else.
+        int n = nums.size();
+
+        long long mini = *min_element(nums.begin(), nums.end()) * n;
+
+        long long sum = accumulate(nums.begin(), nums.end(), 0);
+
+        return (sum - mini);
+    }
+};
