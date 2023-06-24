aaba6dd4891c36e6d25c233f318a6a493ac3e118
AdityaPatil-AP <adityapatil15963@gmail.com>
2023-06-24 16:01:54 +0530

    commit

--- Learning/Leet_Code_Solutions/DynamicProgramming/1027.longest-arithmetic-sequence.cpp
+++ Learning/Leet_Code_Solutions/DynamicProgramming/1027.longest-arithmetic-sequence.cpp
@@ -1,17 +1,28 @@
-class Solution
-{
+class Solution {
 public:
-    int longestArithSeqLength(vector<int> &A)
-    {
-        int res = 2, n = A.size();
-        vector<vector<int>> dp(n, vector<int>(2000, 0));
-        for (int i = 0; i < n; ++i)
-            for (int j = i + 1; j < n; ++j)
-            {
-                int d = A[j] - A[i] + 1000;
-                dp[j][d] = max(2, dp[i][d] + 1);
-                res = max(res, dp[j][d]);
+    
+    int longestArithSeqLength(vector<int>& nums) {
+        // Key Point
+        
+        // 1) Longest Arithmetic Subsequence ending at i with difference equal to k. (-1000 to 1000.).
+        
+        // 2) Instead of using a map which gives TLE. You can use a vector of suitable size.
+        
+        int ans = 2;
+        int n = nums.size();
+        vector<vector<int>> mp(n, vector<int>(2000, 0));
+        // unordered_map<int, unordered_map<int, int>> mp;// for every index storing the longest arithmetic subsequence for the difference which 
+        // occurs with the previous elements in the array.
+        
+        for(int i = 1;i < n;i++){
+            for(int j = 0;j < i;j++){
+                int diff = nums[i] - nums[j] + 1000;
+                mp[i][diff] = (1 + mp[j][diff]);
+                ans = max(ans, mp[i][diff] + 1);
             }
-        return res;
+        }
+        
+        return ans;
+
     }
 };
