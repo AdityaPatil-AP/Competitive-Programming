ebfe5c6b8baadcf106eeadb12498ad0d3c5beb7c
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-06-26 00:03:10 +0530

    commit

--- Learning/Leet_Code_Solutions/DynamicProgramming/1575.count-all-possible-routes.cpp
+++ Learning/Leet_Code_Solutions/DynamicProgramming/1575.count-all-possible-routes.cpp
@@ -7,10 +7,53 @@ public:
 
     int countRoutes(vector<int> &locations, int start, int finish, int fuel)
     {
-        memset(dp, -1, sizeof(dp));
-        int ans = helper(start, finish, fuel, locations);
+        //         memset(dp, -1, sizeof(dp));
+        //         int ans = helper(start, finish, fuel, locations);
 
-        return ans;
+        //         return ans;
+
+        // In State Formation terms.
+
+        // I can define a state as,
+
+        // dp[i][j] -> Number of routes to go from city i to city finish when you have j amount of
+        //             fuel remaining.
+
+        // Formation of valid State Transition Equation.
+        //
+        // dp[i][j] -> (dp[i][j] + dp[k][j - abs(locations[j] - locations[k])]) % mod; // where k is some other city.
+
+        // Base Case, dp[finish][j] = 1; j >= 0.
+
+        // Final Subproblem -> dp[start][fuel].
+        int n = locations.size();
+
+        vector<vector<long long>> dp(n + 1, vector<long long>(fuel + 1, 0));
+
+        // Base Case.
+        for (int j = 0; j <= fuel; j++)
+        {
+            dp[finish][j] = 1;
+        }
+
+        for (int j = 0; j <= fuel; j++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    if (i != k)
+                    {
+                        if (abs(locations[i] - locations[k]) <= j)
+                        {
+                            dp[i][j] = (dp[i][j] + dp[k][j - abs(locations[i] - locations[k])]) % mod;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[start][fuel];
     }
 
     int helper(int start, int finish, int fuel, vector<int> &locations)
